45c52f63f7d11c878a9db37cea75a28b276a5411
YoSpace <yo.space.empire@gmail.com>
2024-09-15 05:15:17 +0300

    ui fixes

--- components/MainContent.js
+++ components/MainContent.js
@@ -16,7 +16,11 @@ const MainContent = () => {
 
     return <section className={styles.main}>
         <div>
-            <img data-aos="fade-down" className={styles.image} src={'/images/logo.png'} />
+            <img
+                data-aos="fade-down"
+                className={styles.image}
+                src={'/images/logo.png'}
+            />
             <div data-aos="zoom-in">
                 <h1 className={styles.title}>
                     YoFoundation
@@ -28,11 +32,19 @@ const MainContent = () => {
         </div>
 
         <div className={styles.showcaseContainer}>
-            <div data-aos="fade-right" className={styles.card}>
+            <div
+                data-aos="fade-right"
+                data-aos-offset="1"
+                className={styles.card}
+            >
                 <div className={styles.number}>$2800+</div>
                 <div className={styles.subtitle}>invested in kindness</div>
             </div>
-            <div data-aos="fade-left" className={styles.card}>
+            <div
+                data-aos="fade-left"
+                data-aos-offset="1"
+                className={styles.card}
+            >
                 <div className={styles.number}>50+</div>
                 <div className={styles.subtitle}>happier people</div>
             </div>
